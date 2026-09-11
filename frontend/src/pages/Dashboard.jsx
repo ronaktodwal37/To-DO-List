19777dc44efda129f22d4a61a83cbd0bf1749978
ronaktodwal37 <ronaktodwal37@gmail.com>
2026-09-11 20:16:37 +0530

    statusfilter

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -13,7 +13,7 @@ const Dashboard = () => {
 
     // Filters and Sorting
     // const [search, setSearch] = useState('');
-    const [statusFilter, setStatusFilter] = useState('All');
+    // const [statusFilter, setStatusFilter] = useState('All');
     const [priorityFilter, setPriorityFilter] = useState('All');
     const [sortBy, setSortBy] = useState('dueDate');
 
